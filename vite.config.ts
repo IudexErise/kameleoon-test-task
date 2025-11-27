d06227f24d53c61dbc1bdd06bbec01b30856d511
Vyacheslav Saprykin <101091013+IudexErise@users.noreply.github.com>
2025-11-27 17:33:19 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://iudexerise.github.io/kameleoon-test-task/'
+  base: '/kameleoon-test-task/'
 });
